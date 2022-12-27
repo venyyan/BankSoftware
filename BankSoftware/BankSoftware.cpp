bbbcfa7e73b61f7ebfe8ecd8bcbff9f73798824f
venyyan <venian2003@abv.bg>
2022-12-27 15:35:15 +0200

    initializing first commands and opening of program

--- BankSoftware/BankSoftware.cpp
+++ BankSoftware/BankSoftware.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+const char COMMAND_LOGIN = 'L';
+const char COMMAND_REGISTER = 'R';
+const char COMMAND_QUIT = 'Q';
+
+
+const string OPENING_MESSAGE = "Hello! You are now using BankSoftware.\nIf you want to Log In type \"L\"\n"
+"If you want to Register type \"R\" \nIf you want to Quit type \"Q\"";
 
 int main()
 {
+	cout << OPENING_MESSAGE;
 
+	char command = ' ';
+
+	while (command != COMMAND_QUIT) {
+		cin >> command;
+
+		
+	}
 }
