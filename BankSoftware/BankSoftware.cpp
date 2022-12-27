307e8c49c86545fa10a38117097082136b5864ca
venyyan <venian2003@abv.bg>
2022-12-28 01:00:32 +0200

    login logic done for now

--- BankSoftware/BankSoftware.cpp
+++ BankSoftware/BankSoftware.cpp
@@ -13,7 +13,10 @@ const string MESSAGE_OPENING = "Hello! You are now using BankSoftware.\nIf you w
 const string MESSAGE_NOT_VALID_USERNAME = "Not a valid username!";
 const string MESSAGE_NOT_VALID_PASSWORD = "Not a valid password!";
 const string MESSAGE_USER_ALREADY_EXISTS = "User already exists!";
-const string MESSAGE_AFTER_LOGGING = "Registered successfully!\nYou have 0 BGN. Choose one of"
+const string MESSAGE_PASSWORD_DOES_NOT_MATCH_USERNAME = "Password does not match username. Try again.";
+const string MESSAGE_USER_DOES_NOT_EXIST = "User does not exist! Try again or register.";
+const string MESSAGE_AFTER_LOGGING_FIRST = "Registered successfully!\nYou have ";
+const string MESSAGE_AFTER_LOGGING_SECOND =  "BGN. Choose one of"
 "the following options: \nC - cancel account\nD - deposit\nL - logout\nT - transfer\nW - withdraw";
 
 
@@ -27,9 +30,12 @@ const unsigned MINIMUM_PASSWORD_LENGTH = 5;
 
 
 string registerUser(string, string);
+string loginUser(string, string);
 bool isValidUsername(string);
 bool isValidPassword(string);
 bool userAlreadyExists(string);
+bool passwordMatchesUsername(string, string);
+vector<string> returnUser(string, string);
 
 vector<vector<string>> userData;
 
@@ -43,7 +49,11 @@ int main()
 		cin >> command;
 
 		if (command == COMMAND_LOGIN) {
-			//login logic here
+			string username;
+			string password;
+			cin >> username >> password;
+			cout << loginUser(username, password);
+
 		}
 		else if (command == COMMAND_REGISTER) {
 			string username;
@@ -67,12 +77,40 @@ string registerUser(string username, string password) {
 	hash<string> passwordHasher;
 	string hashedPassword = to_string(passwordHasher(password));
 
+
 	vector<string> user = { username, hashedPassword, "0" };
 	userData.push_back(user);
 
-	return MESSAGE_AFTER_LOGGING;
+	return MESSAGE_AFTER_LOGGING_FIRST + "0" + MESSAGE_AFTER_LOGGING_SECOND;
 }
 
+string loginUser(string username, string password) {
+	hash<string> passwordHasher;
+	string hashedPassword = to_string(passwordHasher(password));
+	if (!userAlreadyExists(username)) {
+		return MESSAGE_USER_DOES_NOT_EXIST;
+	}
+
+	if (!passwordMatchesUsername(username, hashedPassword)) {
+		return MESSAGE_PASSWORD_DOES_NOT_MATCH_USERNAME;
+	}
+
+	vector<string> loggedUser = returnUser(username, password);
+	string currentBalance = loggedUser[2];
+
+	return MESSAGE_AFTER_LOGGING_FIRST + currentBalance + MESSAGE_AFTER_LOGGING_SECOND;
+}
+
+vector<string> returnUser(string username, string password) {
+	vector<string> user;
+	for (int i = 0; i < userData.size(); i++)
+	{
+		if (userData[i][0] == username && userData[i][1] == password) {
+			user = { username, password, userData[i][2] };
+		}
+	}
+	return user;
+}
 bool isValidUsername(string username) {
 	if (!regex_match(username, regex(REGEX_USERNAME))) {
 		return false;
@@ -104,4 +142,13 @@ bool userAlreadyExists(string username) {
 	return false;
 }
 
+bool passwordMatchesUsername(string username, string password) {
+	for (int i = 0; i < userData.size(); i++)
+	{
+		if (userData[i][0] == username && userData[i][1] == password) {
+			return true;
+		}
+	}
+	return false;
 
+}
