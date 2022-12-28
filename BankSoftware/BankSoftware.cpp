c97c14eb21e19734b0f4300c87d7398b6ca02255
venyyan <venian2003@abv.bg>
2022-12-29 00:23:54 +0200

    login logic done cancel account logic done

--- BankSoftware/BankSoftware.cpp
+++ BankSoftware/BankSoftware.cpp
@@ -6,6 +6,10 @@ using namespace std;
 const char COMMAND_LOGIN = 'L';
 const char COMMAND_REGISTER = 'R';
 const char COMMAND_QUIT = 'Q';
+const char COMMAND_CANCEL_ACCOUNT = 'C';
+const char COMMAND_LOGOUT = 'L';
+const char COMMAND_TRANSFER = 'T';
+const char COMMAND_WITHDRAW = 'W';
 
 
 const string MESSAGE_OPENING = "Hello! You are now using BankSoftware.\nIf you want to Log In type \"L\"\n"
@@ -18,6 +22,7 @@ const string MESSAGE_USER_DOES_NOT_EXIST = "User does not exist! Try again or re
 const string MESSAGE_AFTER_LOGGING_FIRST = "Registered successfully!\nYou have ";
 const string MESSAGE_AFTER_LOGGING_SECOND =  "BGN. Choose one of"
 "the following options: \nC - cancel account\nD - deposit\nL - logout\nT - transfer\nW - withdraw";
+const string MESSAGE_ENTER_PASSWORD = "Please, enter your password: ";
 
 
 
@@ -31,11 +36,13 @@ const unsigned MINIMUM_PASSWORD_LENGTH = 5;
 
 string registerUser(string, string);
 string loginUser(string, string);
+string cancelAccount(vector<string>);
 bool isValidUsername(string);
 bool isValidPassword(string);
 bool userAlreadyExists(string);
 bool passwordMatchesUsername(string, string);
 vector<string> returnUser(string, string);
+
 
 vector<vector<string>> userData;
 
@@ -60,6 +67,9 @@ int main()
 			string password;
 			cin >> username >> password;
 			cout << registerUser(username, password);
+		}
+		else if (command == COMMAND_CANCEL_ACCOUNT) {
+
 		}
 	}
 }
@@ -99,6 +109,31 @@ string loginUser(string username, string password) {
 	string currentBalance = loggedUser[2];
 
 	return MESSAGE_AFTER_LOGGING_FIRST + currentBalance + MESSAGE_AFTER_LOGGING_SECOND;
+}
+
+string cancelAccount(vector<string> user) {
+	string password;
+	cout << MESSAGE_ENTER_PASSWORD;
+	cin >> password;
+
+	//make func to hash passwords
+	string username = user[0];
+	hash<string> passwordHasher;
+	string hashedPassword = to_string(passwordHasher(password));
+
+	if (!passwordMatchesUsername(username, hashedPassword)) {
+		return MESSAGE_PASSWORD_DOES_NOT_MATCH_USERNAME;
+	}
+
+	int position = 0;
+	for (int i = 0; i < userData.size(); i++) {
+		if (userData[i] == user) {
+			position = i;
+		}
+	}
+
+	userData.erase(userData.begin() + position - 1);
+	return MESSAGE_OPENING;
 }
 
 vector<string> returnUser(string username, string password) {
